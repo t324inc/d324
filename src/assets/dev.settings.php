73f7fbfe469cb1e56f93000ccfe71c4db11fd94b
Brian Nowell <briannowell@gmail.com>
2019-05-17 18:52:50 -0700

    Making file references absolute in settings

--- src/assets/dev.settings.php
+++ src/assets/dev.settings.php
@@ -9,8 +9,10 @@ $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['skip_permissions_hardening'] = TRUE;
 
 // Change kint maxLevels setting:
-include_once(DRUPAL_ROOT . '/modules/contrib/devel/kint/kint/Kint.class.php');
-if(class_exists('Kint')){
-  // Set the maxlevels to prevent out-of-memory. Currently there doesn't seem to be a cleaner way to set this:
-  Kint::$maxLevels = 5;
+if(file_exists($app_root . '/modules/contrib/devel/kint/kint/Kint.class.php')) {
+  include_once($app_root . '/modules/contrib/devel/kint/kint/Kint.class.php');
+  if(class_exists('Kint')){
+    // Set the maxlevels to prevent out-of-memory. Currently there doesn't seem to be a cleaner way to set this:
+    Kint::$maxLevels = 5;
+  }
 }
